8cddce8eeaa6a3a64017093b0ee907884c1a0484
Iurie Chigai <124267735+IurieChi@users.noreply.github.com>
2024-03-17 16:19:21 +0100

    added new functionality get_job_type to chec kif word hibrid exist and replace it with hybrid

--- sites/__utils/get_job_type.py
+++ sites/__utils/get_job_type.py
@@ -14,9 +14,16 @@ def get_job_type(sentence: str ,**kwargs) -> str:
     jobs_type = ['hybrid', 'remote', 'on-site']#job_type.lower()
     jobs_type.extend(kwargs.get('jobs_type', []))
     
-    types =  [jobtype for jobtype in jobs_type if jobtype in sentence.lower()]
+    #chec if word hibrid is present and replace it with hybrid
+    if 'hibrid' in sentence.lower():
+        hibrid_sentence = sentence.lower().replace('hibrid', 'hybrid')
+        types =  [jobtype for jobtype in jobs_type if jobtype in hibrid_sentence.lower()]
+    else:
+        types =  [jobtype for jobtype in jobs_type if jobtype in sentence.lower()]
     
     if  len(types) == 0:
         types = ['on-site'] 
    
     return list(set(types))
+
+
