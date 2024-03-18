1e5480ef00479b0df097474c1bce1995a0a0d5e2
Iurie Chigai <124267735+IurieChi@users.noreply.github.com>
2024-03-18 11:52:04 +0100

    fixed getJob_type.py

--- sites/__utils/get_job_type.py
+++ sites/__utils/get_job_type.py
@@ -5,25 +5,26 @@
 #
 #
 
-def get_job_type(sentence: str ,**kwargs) -> str:
+def get_job_type(sentence: str ,**args) -> str:
     '''
-        this func return  a list of job types mentioned in the sendance;
-        **kwargs : Additional keywords arguments.
+        this func return  a list of job types mentioned in the sentence;
+        **args : Additional keywords arguments.
             jobs_type (list):  Additional job types to consider
     '''
-    jobs_type = ['hybrid', 'remote', 'on-site']#job_type.lower()
-    jobs_type.extend(kwargs.get('jobs_type', []))
+    jobs_type = ['hybrid', 'remote', 'on-site']
+    jobs_type.extend(args.get('jobs_type', []))
     
-    #chec if word hibrid is present and replace it with hybrid
+    #check if word hibrid is present and replace it with hybrid to solve typo 
     if 'hibrid' in sentence.lower():
         hibrid_sentence = sentence.lower().replace('hibrid', 'hybrid')
         types =  [jobtype for jobtype in jobs_type if jobtype in hibrid_sentence.lower()]
     else:
         types =  [jobtype for jobtype in jobs_type if jobtype in sentence.lower()]
     
-    if  len(types) == 0:
+    # return by default on-site if function is called with ''
+    if  len(types) == 0: 
         types = ['on-site'] 
-   
+   # return clean data to avoid duplicates
     return list(set(types))
 
 
