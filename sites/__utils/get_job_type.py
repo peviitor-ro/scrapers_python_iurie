6fd08a8143d596f4731688160a015383dc2ace1f
Iurie Chigai <124267735+IurieChi@users.noreply.github.com>
2024-03-20 11:53:30 +0100

    updated get job type

--- sites/__utils/get_job_type.py
+++ sites/__utils/get_job_type.py
@@ -6,13 +6,13 @@
 #
 
 def get_job_type(sentence: str ,**additional_job_types) -> str:
-    '''
-        this func return  a list of job types mentioned in the sentence;
-        **args : Additional keywords arguments.
-            jobs_type (list):  Additional job types to consider
-    '''
+    """
+    This func return  a list of job types mentioned in the sentence;
+    **args : Additional keywords arguments.
+    jobs_type (list):  Additional job types to consider
+    """
     jobs_type = ['hybrid', 'remote', 'on-site', 'hibrid']
-    jobs_type.extend(additional_job_types)#.get('jobs_type', []))
+    jobs_type.extend(additional_job_types)
     
     types = set([jobtype for jobtype in jobs_type if jobtype in sentence.lower()])
     
