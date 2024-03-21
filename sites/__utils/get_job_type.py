b7ec230c9c3027ff71a7113f002d1b4c80aae3a4
Iurie Chigai <124267735+IurieChi@users.noreply.github.com>
2024-03-21 11:22:18 +0100

    updated logic for get job type

--- sites/__utils/get_job_type.py
+++ sites/__utils/get_job_type.py
@@ -5,13 +5,12 @@
 #
 #
 
-def get_job_type(sentence: str ,**additional_job_types) -> str:
+def get_job_type(sentence: str ,*kwargs) -> str:
     """
     This func return  a list of job types mentioned in the sentence;
-    **additional_job_types : Additional keywords arguments.
+    *kwargs : Additional keywords arguments for job type that is not mentioned.
     """
     jobs_type = ['hybrid', 'remote', 'on-site', 'hibrid']
-    jobs_type.extend(additional_job_types)
     lower_sentance = sentence.lower()
     
     types = set([jobtype for jobtype in jobs_type if jobtype in lower_sentance])
@@ -25,6 +24,10 @@ def get_job_type(sentence: str ,**additional_job_types) -> str:
         types.remove('hibrid')
         types.add('hybrid')
     
-    return list(types)
-
-
+    types = list(types)
+    
+    if len(kwargs)>0:
+        add_job_type = [''.join(map(str,kwargs))]
+        types.extend(add_job_type)
+        
+    return types
