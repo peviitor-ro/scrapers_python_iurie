ac25cb94472130fc8983c3ad5af755564a26caf2
Iurie Chigai <124267735+IurieChi@users.noreply.github.com>
2024-03-22 17:08:39 +0100

    fixed location logic for Webasto

--- sites/voxility_scraper.py
+++ sites/voxility_scraper.py
@@ -37,19 +37,19 @@ def scraper():
             
        #extract location from job_location and replace bucharest to Bucuresti
         if (location := job.find('span', attrs={'job-location'}).text.split()[-2].replace(',','').lower() in ['bucharest']):
-            location ='București'
+            location = 'București'
         
-        finish_location=get_county(location)
+        finish_location = get_county(location)
 
         # get jobs items from response
         job_list.append(Item(
             job_title = job.find('a').text,
             job_link = base_link + job.find('a',)['href'].replace('..',''),
-            company='Voxility',
-            country='Romania',
+            company = 'Voxility',
+            country = 'Romania',
             county = finish_location[0] if True in finish_location  else None,
-            city='all' if location.lower() == finish_location[0].lower() and finish_location[0].lower() !='bucuresti' else finish_location[0],
-            remote = get_job_type(job.find('a').text.lower()),
+            city = 'all' if True in finish_location and finish_location[0] !='Bucuresti' else finish_location[0],
+            remote = get_job_type(job.find('a').text),
         ).to_dict())
 
     return job_list
