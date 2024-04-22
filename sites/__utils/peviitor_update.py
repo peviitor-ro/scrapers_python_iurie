33f8c3247e92862ffcdd6f8db2a9ee22d43fa420
Iurie Chigai <124267735+IurieChi@users.noreply.github.com>
2024-04-22 13:59:36 +0200

    Updatede function on file peviitor_update

--- sites/__utils/peviitor_update.py
+++ sites/__utils/peviitor_update.py
@@ -7,60 +7,25 @@
 #
 import requests
 #
-import os  # I do not have API KEY
-#
 import json
-#
-import time
 
-EMAIL = 'chigaiiura@yahoo.com'
-DOMAIN = 'https://api.peviitor.ro/v5/'
-
-TOKEN_ROUTE = 'get_token/'
-ADD_JOBS_ROUTE = 'add/'
 
 class UpdateAPI:
     '''
-    - Method for clean data,
     - Method for update data,
     - Method for update logo.
     '''
 
     def __init__(self):
-        self.api_key = os.environ.get('IURIE')
-        self.clean_url = 'https://api.peviitor.ro/v4/clean/'
-        self.post_url = 'https://api.peviitor.ro/v4/update/'
-        self.logo_url = 'https://api.peviitor.ro/v1/logo/add/'
+        self.logo_url = 'https://api.peviitor.ro/v1/logo/add/'   
+        self.logo_header = { 'Content-Type': 'application/json'}
         
-        
-        self.clean_header = {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'apikey': self.api_key
-        }
+        self.EMAIL = 'chigaiiura@yahoo.com'
+        self.DOMAIN = 'https://api.peviitor.ro/v5/'
 
-        self.post_header = {
-            'Content-Type': 'application/json',
-            'apikey': self.api_key
-        }
+        self.TOKEN_ROUTE = 'get_token/'
+        self.ADD_JOBS_ROUTE = 'add/'
 
-        self.logo_header = {
-            'Content-Type': 'application/json',
-        }
-
-    def update_jobs(self, company_name: str, data_jobs: list):
-        ''' update and clean data on peviitor'''
-        clean_request = requests.post(self.clean_url, headers=self.clean_header, data={'company': company_name})
-
-        # time sleep for SOLR indexing
-        time.sleep(0.2)
-
-        
-        post_request_to_server = requests.post(self.post_url, headers=self.post_header, data=json.dumps(data_jobs))
-
-      
-        #######################################################################
-
-        print(json.dumps(data_jobs, indent=4))
 
     def update_logo(self, id_company: str, logo_link: str):
         '''update logo on peviitor.ro'''
@@ -75,23 +40,28 @@ class UpdateAPI:
         Returnează token-ul necesar pentru a face request-uri către API.
         :return: token-ul necesar pentru a face request-uri către API
         """
-        endpoint = TOKEN_ROUTE
-        email = EMAIL
-        url = f"{DOMAIN}{endpoint}"
+        endpoint = self.TOKEN_ROUTE
+        email = self.EMAIL
+        url = f"{self.DOMAIN}{endpoint}"
         response = requests.post(url, json={"email": email})
         return response.json()["access"]
 
     def publish(self, data):
-        route = ADD_JOBS_ROUTE
-        url = f"{DOMAIN}{route}"
+        """This method publish scraped data using API to Validator with post method
+        using token to make API request.
+        Args:
+            data (_type_): Json
+        """
+        
+        route = self.ADD_JOBS_ROUTE
+        url = f"{self.DOMAIN}{route}"
         token = self.get_token()
 
         headers = {"Content-Type": "application/json",
                 "Authorization": f"Bearer {token}"}
 
         requests.post(url, headers=headers, json=data)
+       
+        print(json.dumps(data, indent=4))
         
-        
-# token endpoint = "https://api.laurentiumarian.ro/get_token" genereaza un nou token. body {"email":"emailultau"}
-# adaugare joburi endpoint = "https://api.laurentiumarian.ro/jobs/add/"
-# ambele metoda post
+    
