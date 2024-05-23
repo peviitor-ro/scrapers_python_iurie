e679c584c1259fdd472a21b6263a03bad2ce4873
Iurie Chigai <124267735+IurieChi@users.noreply.github.com>
2024-05-23 17:05:24 +0200

    all

--- sites/__utils/default_headers.py
+++ sites/__utils/default_headers.py
@@ -9,7 +9,7 @@
 
 DEFAULT_HEADERS = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-    'Accept-Language': 'en-US,en;q=0.5',
-    'Refer': 'https://google.com',
-    'DNT': '1'
+    # 'Accept-Language': 'en-US,en;q=0.5',
+    # 'Refer': 'https://google.com',
+    # 'DNT': '1'
 }
