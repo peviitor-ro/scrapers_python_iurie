4eb259d667dd806c6ac4b84a30b907140851d5a3
Iurie Chigai <124267735+IurieChi@users.noreply.github.com>
2024-07-15 14:15:42 +0200

    fixed 404 Error on Job Links After Synchronizing Jobs for Chorus

--- sites/chorus.py
+++ sites/chorus.py
@@ -26,37 +26,33 @@ def scraper():
     """
     ... scrape data from Chorus scraper.
     """
-    soup = GetStaticSoup("https://www.chorus.ro/job_list")
+    soup = GetStaticSoup("https://www.chorus.ro/Careers/Careers")
 
     job_list = []
-    for job in soup.find_all('ul', attrs=('descr')):
-        for data in job.find_all('li'):
-            #extract location from page 
-            br_tag = data.find('br') #find <br> and assigne it to variable
-            if br_tag:
-                #create a list of locations
-                locations = br_tag.next_sibling.strip().split(', ') 
-                
-                #replace incorect location from a list
-                if 'MogosoaiaSe' in locations[0]:
-                    locations[0] = 'Mogosoaia'
-                    locations.append('Bucuresti')
-                
-                #extract county for locations 
-                check_county=[get_county_json(county) for county in locations ]
-                clean_county=[item for sublist in check_county for item in sublist]
-                unic_county=list(set(clean_county))
-            
-                # get jobs items from response
-                job_list.append(Item(
-                    job_title = data.find('a').text.strip(),
-                    job_link = data.find('a')['href'],
-                    company = 'Chorus',
-                    country = 'România',
-                    county = unic_county,
-                    city = locations,
-                    remote = 'on-site',
-                ).to_dict())
+    for job in soup.find_all('div', class_='card-body px-lg-4 d-flex flex-column justify-content-between'):
+        
+        location=job.find("h6", class_="card-subtitle mb-2 text-muted").text.strip()
+        #clean location string
+        if '(Se recruteaza si din:' in location:
+            location=location.replace(" (Se recruteaza si din:", ",").replace("(", "").replace(")", "").strip()
+        locations=location.strip().split(", ")
+
+        #extract county for locations 
+        check_county=[get_county_json(county) for county in locations ]
+        clean_county=[item for sublist in check_county for item in sublist]
+        unic_county=list(set(clean_county))
+        
+    
+        # get jobs items from response
+        job_list.append(Item(
+            job_title = job.find("h5", class_="card-title primary-color-c").text,
+            job_link = "https://www.chorus.ro"+job.find_all("a")[-1].get("href"),
+            company = 'Chorus',
+            country = 'România',
+            county = unic_county,
+            city = locations,
+            remote = 'on-site',
+        ).to_dict())
 
     return job_list
 
@@ -69,10 +65,10 @@ def main():
     """
 
     company_name = "Chorus"
-    logo_link = "https://www.chorus.ro/images/design/logo.png"
+    logo_link = "https://www.chorus.ro/images/logo.png?v=zXU74pBM8ngL6cEDeYM7AN61hnFBOVzAVCyZSWCxjng"
 
     jobs = scraper()
-    print(len(jobs))
+    print("jobs found",len(jobs))
     # uncomment if your scraper done
     UpdateAPI().publish(jobs)
     UpdateAPI().update_logo(company_name, logo_link)
