8536b9299bab0c0018a8dd08f2e442ad11a8aee0
Iurie Chigai <124267735+IurieChi@users.noreply.github.com>
2024-11-14 12:31:41 +0100

    optimized code for mediagalaxy

--- sites/mediagalaxy.py
+++ sites/mediagalaxy.py
@@ -15,7 +15,7 @@
 #
 from bs4 import BeautifulSoup
 from __utils import (
-    RequestsCustum,
+    GetCustumRequest,
     get_county,
     get_county_json,
     get_job_type,
@@ -26,35 +26,36 @@ from __utils import (
 
 def scraper():
     """ scrape data from Mediagalaxy scraper """
-    
+
     url = "https://mediagalaxy.ro/cariere/"
 
     payload = {}
     headers = {
-            'sec-ch-ua': '"Not)A;Brand";v="99", "Brave";v="127", "Chromium";v="127"',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36'
-            }
-    data = RequestsCustum(url, headers, payload)
-    # transfom text responce in soup object
-    soup =  BeautifulSoup(data, 'lxml')
-
+        'sec-ch-ua': '"Not)A;Brand";v="99", "Brave";v="127", "Chromium";v="127"',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36'
+    }
+    response = GetCustumRequest(url, headers, payload)
+   
     job_list = []
-    for job in soup.find_all("div",class_="border rounded px-8"):
+    for job in response.find_all("div", class_="border rounded px-8"):
 
-        title=job.find('h2', class_="flex-shrink-0 mb-3 md:mb-0 md:w-64 md:pr-6 font-medium capitalize").text
-        link=title.replace(" ","-")
-        location=job.find('div', class_="w-full mb-2 md:mb-0").text.split(':')[1].strip().split(', ')
-        county_finish=["Cluj"if city =="Cluj" else get_county_json(city)[0] for city in location]
-       
+        title = job.find(
+            'h2', class_="flex-shrink-0 mb-3 md:mb-0 md:w-64 md:pr-6 font-medium capitalize").text
+        link = title.replace(" ", "-")
+        location = job.find(
+            'div', class_="w-full mb-2 md:mb-0").text.split(':')[1].strip().split(', ')
+        county_finish = ["Cluj"if city == "Cluj" else get_county_json(city)[
+            0] for city in location]
+
         # get jobs items from response
         job_list.append(Item(
             job_title=title.capitalize(),
             job_link="https://mediagalaxy.ro/cariere/#" + link,
             company="Mediagalaxy",
             country="România",
-            county = county_finish,
-            city = location,
-            remote = "on-site",     
+            county=county_finish,
+            city=location,
+            remote="on-site",
         ).to_dict())
 
     return job_list
@@ -71,7 +72,7 @@ def main():
     logo_link = "https://bucurestimall.ro/wp-content/uploads/2016/12/MediaGalaxy.jpg"
 
     jobs = scraper()
-    print("jobs found:",len(jobs))
+    print("jobs found:", len(jobs))
     # uncomment if your scraper done
     UpdateAPI().publish(jobs)
     UpdateAPI().update_logo(company_name, logo_link)
