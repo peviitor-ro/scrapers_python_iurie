478af7d567cbfee4acd32d8ccc04354ff71078d2
Iurie Chigai <124267735+IurieChi@users.noreply.github.com>
2024-12-05 12:51:04 +0100

    fixed  location and county bug for Engie #245

--- sites/engie.py
+++ sites/engie.py
@@ -31,52 +31,70 @@ def scraper():
     page = 0
     flag = True
     job_list = []
-    
+
     while flag:
-        soup = GetStaticSoup(f"https://jobs.engie.com/search/?q=&locationsearch=Romania&startrow={page}")
+        soup = GetStaticSoup(
+            f"https://jobs.engie.com/search/?q=&locationsearch=Romania&startrow={page}")
         # print(soup)
-        if len(jobs := soup.find_all('tr', attrs=('data-row'))) >0:
+        if len(jobs := soup.find_all('tr', class_=('data-row'))) > 0:
             for job in jobs:
-            
+
+                # Initialize `cities` as an empty list to ensure it's always defined
+                cities = []
+
                 # Remove "Romania" from the location --- Start
-                city_location = job.find('span', attrs ='jobLocation').text.strip().title().split(', R')[0].split(', ')
-    
-                # corect location typo    
-                for city in range(len(city_location)):
-                    
-                    if 'Com.Blejoi' in city_location[city]:
-                        city_location[city]='Blejoi'
-                    if 'Bucharest' in city_location[city]:
-                        city_location[city]='Bucuresti'
-                    if 'Ploiest' in city_location[city]:
-                        city_location[city] = 'Ploiesti'
-                    if 'Turnu Mag' in  city_location[city]:
-                        city_location[city] = 'Turnu Magurele'
-                    if "Targu Mures" in city_location[city]:
-                        city_location[city]="Targu-Mures"
-              
-                # check county for cities from city_loc list  add to a county list if True else not then None 
-                job_county = [get_county_json(city)[0] for city in city_location]
+                city_location = job.find(
+                    'span', class_='jobLocation').text.strip()
+
+                # Handle locations with different delimiters
+                if ', Ro' in city_location or ', R' in city_location:
+                    # Split on commas
+                    cities = city_location.split(', R')[0].split(', ')
+
+                if " / " in city_location:
+                    cities = city_location.split(', R')[0].split(
+                        ' / ')  # Split on slashes
+
+                # corect location
+                for city in range(len(cities)):
+
+                    if 'Com.Blejoi' in cities[city]:
+                        cities[city] = 'Blejoi'
+                    if 'Bucharest' in cities[city]:
+                        cities[city] = 'Bucuresti'
+                    if 'Ploiest' in cities[city]:
+                        cities[city] = 'Ploiesti'
+                    if 'Turnu Mag' in cities[city]:
+                        cities[city] = 'Turnu Magurele'
+                    if "Targu Mures" in cities[city]:
+                        cities[city] = "Targu-Mures"
+                    if "Pi" in cities[city]:
+                        cities[city] = "Pitesti"
+                    if "C" in cities[city]:
+                        cities[city] = "Constanța"
+
+                # check county for cities  add to a county list
+                job_county = [get_county_json(city)[0]for city in cities]
                 
                 # get jobs items from response
                 job_list.append(Item(
-                    job_title = job.find('a', attrs='jobTitle-link').text,
-                    job_link='https://jobs.engie.com'+ job.find('a')['href'],
+                    job_title=job.find('a', class_='jobTitle-link').text,
+                    job_link='https://jobs.engie.com' + job.find('a')['href'],
                     company='ENGIE',
                     country='România',
-                    county = job_county,
-                    city = city_location,
+                    county=job_county,
+                    city=cities,
                     # for location if all then location remote else On-site
-                    remote = "on-site",
+                    remote="on-site",
                 ).to_dict())
-    
+
         else:
             flag = False
-        
+
         # increment page
         page += 25
         time.sleep(1)
-    
+
     return job_list
 
 
@@ -91,7 +109,7 @@ def main():
     logo_link = "https://rmkcdn.successfactors.com/c4851ec3/1960b45a-f47f-41a6-b1c7-c.svg"
 
     jobs = scraper()
-    print(len(jobs))
+    print("Engie jobs",len(jobs))
     # uncomment if your scraper done
     UpdateAPI().publish(jobs)
     UpdateAPI().update_logo(company_name, logo_link)
