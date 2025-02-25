2e58ff9b65a6ba0bb24a9baaef99e42e695ad47c
Iurie Chigai <124267735+IurieChi@users.noreply.github.com>
2025-02-25 11:02:55 +0100

    fixed Concentrix scraper

--- sites/concentrix.py
+++ sites/concentrix.py
@@ -1,26 +1,26 @@
-#
-#
-#  Basic for scraping data from static pages
-#
-# ------ IMPORTANT! ------
-# if you need return soup object:
-# you cand import from __utils -> GetHtmlSoup
-# if you need return regex object:
-# you cand import from __utils ->
-# ---> get_data_with_regex(expression: str, object: str)
-#
-# Company ---> Concentrix
-# Link ------> https://jobs.concentrix.com/job-search/?keyword=&country=Romania
-#
-#
+"""
+Basic for scraping data from static pages
+
+------ IMPORTANT! ------
+if you need return soup object:
+you cand import from __utils -> GetHtmlSoup
+if you need return regex object:
+you cand import from __utils ->
+---> get_data_with_regex(expression: str, object: str)
+
+Company ---> Concentrix
+Link ------> https://jobs.concentrix.com/job-search/?keyword=&country=Romania
+
+"""
+
 from __utils import (
-   
-    get_county,
+    get_county_json,
     get_job_type,
     Item,
     UpdateAPI,
 )
-import  requests
+import requests
+from requests import HTTPError
 from bs4 import BeautifulSoup
 
 
@@ -28,41 +28,46 @@ def scraper():
     """
     ... scrape data from Concentrix scraper.
     """
-    payload={"action":"gd_jobs_query_pagination",
-               "country":"Romania",
-               "jobs_shown":0,
-               "jobs_per_page":50
-               }
-   
-    headers={
-        "x-requested-with":"XMLHttpRequest"
+    url = "https://jobs.concentrix.com/wp-admin/admin-ajax.php"
+
+    payload = "action=gd_jobs_query_pagination&country%5B%5D=Romania&keyword=&jobs_shown=0&jobs_per_page=50&wh=false"
+
+    headers = {
+        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36",
+        "Cookie": "__cf_bm=Eulwr4cnl4nSz42e4pW6pBBsuP0CpdA8Fgny9LoQyPk-1740473978-1.0.1.1-CRHhcFXSGDtPFw2PAzGTBIV4ZeJvCf9sZ2iodo2KLpp.zOrtHCRJgCz1ObRH34GxaWW2fNqJW.sX45yFkMkICw",
     }
-    url="https://jobs.concentrix.com/wp-admin/admin-ajax.php"
-    responce=requests.post(url=url, data=payload, headers=headers)
-    html=responce.json().get("data").get("output")
-    soup=BeautifulSoup(html,'lxml')
-    # print(responce)
-    job_list = []
-    for job in soup.find_all("div",attrs="job"):
-        title=job.find("h3").text
-        clean_title=title.split(' –')[0].split(" - ")[0]
-        location=job.find("div", attrs="job-location").text.split(', R')[0]
-        # change Bucharest to București
-        if location=="Bucharest":
-            location="București"
-        #check if location is county 
-        check_county=get_county(location)
-        
-        # get jobs items from response
-        job_list.append(Item(
-            job_title=clean_title,
-            job_link=job.find("a")["href"],
-            company="Concentrix",
-            country="România",
-            county = check_county[0] if True in check_county else None,
-            city = 'all' if True in check_county and check_county[0]!='Bucuresti' else location ,
-            remote = get_job_type(title),
-        ).to_dict())
+    try:
+        response = requests.post(url=url, data=payload, headers=headers)
+        html = response.json().get("data").get("output")
+        soup = BeautifulSoup(html, "lxml")
+
+        job_list = []
+        for job in soup.find_all("div", class_="job"):
+            title = job.find("h3").text
+            clean_title = title.split(" –")[0].split(" - ")[0]
+            location = job.find("div", attrs="job-location").text.split(", R")[0]
+            # change Bucharest to București
+            if location == "Bucharest":
+                location = "București"
+            if location == "Targu":
+                location = "Cluj"
+            # get county for location
+            county = get_county_json(location)
+            # get jobs items from response
+            job_list.append(
+                Item(
+                    job_title=clean_title,
+                    job_link=job.find("a")["href"],
+                    company="Concentrix",
+                    country="România",
+                    county=county,
+                    city=location,
+                    remote=get_job_type(title),
+                ).to_dict()
+            )
+    except (HTTPError, ValueError) as e:
+        print("Somethink went wrong", e)
 
     return job_list
 
@@ -78,11 +83,11 @@ def main():
     logo_link = "https://jobs.concentrix.com/wp-content/themes/jobswh/img/logo-concentrix-color.svg"
 
     jobs = scraper()
-    print("jobs found:",len(jobs))
+    print("jobs found:", len(jobs))
     # uncomment if your scraper done
     UpdateAPI().publish(jobs)
     UpdateAPI().update_logo(company_name, logo_link)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
