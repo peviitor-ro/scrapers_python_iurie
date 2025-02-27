605aaa6261eb52f5a21b4f50cef6cf51dc33a7ae
Iurie Chigai <124267735+IurieChi@users.noreply.github.com>
2025-02-27 11:37:24 +0100

    fix sostenia scraper

--- sites/sostenia.py
+++ sites/sostenia.py
@@ -1,18 +1,18 @@
-#
-#
-#  Basic for scraping data from static pages
-#
-# ------ IMPORTANT! ------
-# if you need return soup object:
-# you cand import from __utils -> GetHtmlSoup
-# if you need return regex object:
-# you cand import from __utils ->
-# ---> get_data_with_regex(expression: str, object: str)
-#
-# Company ---> Sostenia
-# Link ------> https://www.sostenia.ro/en/jobs
-#
-#
+"""
+Basic for scraping data from static pages
+
+------ IMPORTANT! ------
+if you need return soup object:
+you cand import from __utils -> GetHtmlSoup
+if you need return regex object:
+you cand import from __utils ->
+---> get_data_with_regex(expression: str, object: str)
+
+Company ---> Sostenia
+Link ------> https://www.sostenia.ro/en/jobs
+
+"""
+
 from __utils import (
     GetStaticSoup,
     get_county,
@@ -30,24 +30,34 @@ def scraper():
     soup = GetStaticSoup("https://www.sostenia.ro/en/jobs")
 
     job_list = []
-    data=soup.find_all("div",attrs="col-lg")
-    for job_grid in data:
-        for job in job_grid.find_all("a"):
-            
-            link="https://www.sostenia.ro/"+job.get('href')
-
-            # get jobs items from response
-            job_list.append(Item(
-                job_title=job.find("h3", attrs="text-secondary mt0 mb4").text.strip(),
+    data = soup.find_all("a", class_="link-decoration-none")
+    for job in data:
+        title = job.find("h3", class_="text-secondary mt0 mb4").text.strip()
+        link = "https://www.sostenia.ro/" + job.get("href")
+        # Find all <li> elements that contain "remote" or "hibrid"
+        result = [
+            li.get_text(strip=True)
+            for li in job.find_all("li")
+            if "remote" in li.text.lower()
+            or "hibrid" in li.text.lower()
+            or "hybrid" in li.text.lower()
+            or "remote/hybrid" in li.text.lower()
+        ]
+        job_type = get_job_type(result[0]) if result else "hybrid"
+        # get jobs items from response
+        job_list.append(
+            Item(
+                job_title=title,
                 job_link=link,
                 company="Sostenia",
                 country="România",
                 county="București",
                 city="București",
-                remote="Hybrid",
-            ).to_dict())
+                remote=job_type,
+            ).to_dict()
+        )
 
-        return job_list
+    return job_list
 
 
 def main():
@@ -58,14 +68,16 @@ def main():
     """
 
     company_name = "Sostenia"
-    logo_link = "https://www.sostenia.ro/web/image/website/1/logo/Sostenia?unique=6994ba0"
+    logo_link = (
+        "https://www.sostenia.ro/web/image/website/1/logo/Sostenia?unique=6994ba0"
+    )
 
     jobs = scraper()
-    print("jobs found:",len(jobs))
+    print("jobs found:", len(jobs))
     # uncomment if your scraper done
     UpdateAPI().publish(jobs)
     UpdateAPI().update_logo(company_name, logo_link)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
