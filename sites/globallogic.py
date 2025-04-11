91fd0abdb428fc8717257ae37076b26808d968a6
Iurie Chigai <124267735+IurieChi@users.noreply.github.com>
2025-04-11 17:19:49 +0200

    fixing globallogic

--- sites/globallogic.py
+++ sites/globallogic.py
@@ -1,18 +1,10 @@
-#
-#
-#  Basic for scraping data from static pages
-#
-# ------ IMPORTANT! ------
-# if you need return soup object:
-# you cand import from __utils -> GetHtmlSoup
-# if you need return regex object:
-# you cand import from __utils ->
-# ---> get_data_with_regex(expression: str, object: str)
-#
-# Company ---> globallogic
-# Link ------> https://www.globallogic.com/career-search-page/page/1/?keywords&experience&locations=romania&c
-#
-#
+"""
+Basic for scraping data from static pages
+
+Company ---> globallogic
+Link ------> https://www.globallogic.com/career-search-page/page/1/?location=romania
+"""
+
 from __utils import (
     GetCustumRequest,
     get_county,
@@ -22,58 +14,71 @@ from __utils import (
     UpdateAPI,
 )
 
+
 def scraper():
     """
     ... scrape data from globallogic scraper.
     """
     page = 1
-    url = f"https://www.globallogic.com/career-search-page/page/1/?keywords&experience&locations=romania&c"
+    url = f"https://www.globallogic.com/career-search-page/page/1/?location=romania"
 
     payload = {}
     headers = {
-        'Cookie': 'incap_ses_324_1279438=qRoLJ4SgbzegytCEVhR/BNKpIGcAAAAAVaTYjgljkvDdmpdyLc1wGw==; visid_incap_1279438=Ps0QFb7pRrW4mEDxOB/F+7+pIGcAAAAAQUIPAAAAAAAzzcXj1EATKZ9nhSZEWJp6; PHPSESSID=68hmh3g23aeflo3etkoov639l4; locations=romania; wordpress_google_apps_login=789a878c2075bd7d640fdc9992f86955'
+        "Cookie": "incap_ses_324_1279438=qRoLJ4SgbzegytCEVhR/BNKpIGcAAAAAVaTYjgljkvDdmpdyLc1wGw==; visid_incap_1279438=Ps0QFb7pRrW4mEDxOB/F+7+pIGcAAAAAQUIPAAAAAAAzzcXj1EATKZ9nhSZEWJp6; PHPSESSID=68hmh3g23aeflo3etkoov639l4; locations=romania; wordpress_google_apps_login=789a878c2075bd7d640fdc9992f86955"
     }
     response = GetCustumRequest(url=url, payload=payload, headers=headers)
 
     job_list = []
     # find  number of pages
-    pages = response.find("span", class_="pages").text.split()[-1]
+    pages = (
+        response.find("div", class_="pagination")
+        .find_all("a", class_="page-numbers")[-2]
+        .text
+    )
 
-    for page in range(2, int(pages)+2):
+    for page in range(2, int(pages) + 2):
 
-        for job in response.find_all("div", class_="career-pagelink"):
-            job_type = job.find("span",  class_="remote-badge").text
-            location = job.find("span", class_="job-locations").text
+        for job in response.find_all("a", class_="job_box"):
 
-            # Check if a specific city is listed after "Romania -"
-            if "Romania -" in location:
-                # Extract city names after "Romania -"
-                city_part = location.split("Romania -", 1)[1].strip()
-                city_list = ["Bucuresti" if "Bucharest" in city.strip(
-                ) else city.strip() for city in city_part.split(',')]
-                cities = city_list
-            else:
-                # If no specific city, add "all"
-                cities = ["all"]
+            data = job.find("div", class_="top_area").text
+            locations = [
+                span.text.strip()
+                for span in job.find_all("span", class_="job_location")
+            ]
+
+            # Remove  "Romania" from the list
+            if "Romania" in locations:
+                if len(locations) > 1:
+                    locations.remove("Romania")
+                else:
+                    locations = "all"
+
+            if "Bucharest" in locations:
+                locations.remove("Bucharest")
+                locations.append("Bucuresti")
 
             # Find county for city
-            county = ["all" if "all" == city else "Iasi" if "Iasi" ==
-                      city else get_county_json(city)[0] for city in cities]
+            if "all" in locations:
+                county = "all"
+            else:
+                county = [get_county_json(city) for city in locations]
 
             # get jobs items from response
-            job_list.append(Item(
-                job_title=job.find("p",  class_="mb-0").text,
-                job_link=job.find("p",  class_="mb-0").find("a")["href"],
-                company="globallogic",
-                country="România",
-                county=county,
-                city=cities,
-                remote=get_job_type(job_type),
-            ).to_dict())
+            job_list.append(
+                Item(
+                    job_title=job.find("h4").text,
+                    job_link=job.get("href"),
+                    company="globallogic",
+                    country="România",
+                    county=county,
+                    city=locations,
+                    remote=get_job_type(data),
+                ).to_dict()
+            )
         # incriment pages and make next request
-        url = f"https://www.globallogic.com/career-search-page/page/{page}/?keywords&experience&locations=romania&c"
+        url = f"https://www.globallogic.com/career-search-page/page/{page}/?location=romania"
         response = GetCustumRequest(url=url, payload=payload, headers=headers)
-        
+
     return job_list
 
 
@@ -94,5 +99,5 @@ def main():
     UpdateAPI().update_logo(company_name, logo_link)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
