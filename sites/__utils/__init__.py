63f9fa8efc288b447891c1d464a7a964d11133bf
Iurie Chigai <124267735+IurieChi@users.noreply.github.com>
2025-04-15 10:16:46 +0200

    added request func

--- sites/__utils/__init__.py
+++ sites/__utils/__init__.py
@@ -1,10 +1,15 @@
-
 import __utils
 from __utils.peviitor_update import UpdateAPI
 from __utils.default_headers import DEFAULT_HEADERS
 from __utils.items_struct import Item
-from __utils.found_county import get_county, get_county_json, counties, update_location_if_is_county
+from __utils.found_county import (
+    get_county,
+    get_county_json,
+    counties,
+    update_location_if_is_county,
+)
 from __utils.req_bs4_shorts import (
+    GetContentDynamicPage,
     GetCustumRequest,
     GetCustumRequestJson,
     PostCustumRequest,
@@ -16,6 +21,7 @@ from __utils.req_bs4_shorts import (
     HackCloudFlare,
     GetXMLObject,
     GetDataCurl,
+    Request,
 )
 from __utils.dynamic_requests_html_shorts import GetDynamicSoup
 from __utils.get_job_type import get_job_type
