bd12eebcdd83bbb9ef6c7fe2b460f3df35aa7c25
Iurie Chigai <chigaiiura@yahoo.com>
2025-11-11 11:26:38 -0800

    fixed location issue with county

--- sites/deloitte.py
+++ sites/deloitte.py
@@ -1,18 +1,13 @@
-#
-#
-#  Basic for scraping data from static pages
-#
-# ------ IMPORTANT! ------
-# if you need return soup object:
-# you cand import from __utils -> GetHtmlSoup
-# if you need return regex object:
-# you cand import from __utils ->
-# ---> get_data_with_regex(expression: str, object: str)
-#
-# Company ---> Deloitte
-# Link ------> https://apply.deloittece.com/en_US/careers/SearchJobs/?523=%5B5509%5D&523_format=1482&listFilterMode=1&jobRecordsPerPage=10&jobOffset=0
-#
-#
+
+"""
+Basic for scraping data from static pages
+
+Company ---> Deloitte
+Link ------> https://apply.deloittece.com/en_US/careers/SearchJobs/?523=%5B5509%5D&523_format=1482&listFilterMode=1&jobRecordsPerPage=10&jobOffset=0
+
+"""
+ 
+
 from __utils import (
     GetStaticSoup,
     get_county,
@@ -41,7 +36,7 @@ def scraper():
     total_results = int(total_jobs.split('of')[-1].split()[0])
     # Calculate the number of pages
     pages = math.ceil(total_results / 10)
-
+    
     for page in range(1, pages+1):
 
         for job in soup.find_all("div", class_="article__header__text"):
@@ -56,9 +51,7 @@ def scraper():
                 location_data = location_data.replace('Bucharest', 'București')
             location = location_data.split()
             # create a list of county base on city
-            check_county = ["Iasi" if city == "Iasi" else get_county_json(city)[
-                0] for city in location]
-
+            check_county = [get_county_json(city) for city in location]
             # get jobs items from respons
             job_list.append(Item(
                 job_title=job.find('a').text.strip(),
@@ -90,7 +83,6 @@ def main():
 
     jobs = scraper()
     print("jobs found:", len(jobs))
-    # uncomment if your scraper done
     UpdateAPI().publish(jobs)
     UpdateAPI().update_logo(company_name, logo_link)
 
